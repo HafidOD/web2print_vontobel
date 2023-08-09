0ee39b4ea165d7d6e7cbc4dff6b620d1fbff67f3
HafidOD <hafido1403@gmail.com>
2023-08-09 13:58:35 -0500

    add email test

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -33,7 +33,8 @@ export async function POST(req) {
 
     const mailOptions = {
       from: process.env.SMTP_USER,
-      to: "hafid@tachuela.mx",
+      to: "hafid@tachuela.mx, lili@tachuela.mx, rocio@tachuela.mx",
+      cc: "hola@tachuela.mx",
       subject: "test de envio de correo",
       html: emailContent,
       // text: `Detalles del pedido:\n\n${JSON.stringify(items)}`,
@@ -57,11 +58,11 @@ function generateEmailContent(items) {
   let content = "<h1>Detalles del Pedido</h1>";
   content += "<table>";
   content +=
-    "<thead><tr><th>Imagen</th><th>Producto</th><th>Moneda</th><th>Cantidad</th><th>Precio</th></tr></thead>";
+    "<thead><tr><th>Imagen</th><th>Producto</th><th>Cantidad</th><th>Precio</th><th>Moneda</th></tr></thead>";
   content += "<tbody>";
 
   items.items.forEach((producto) => {
-    content += `<tr><td><img src=${process.env.NEXT_URL_BASE}${producto.imageProduct} width="140" alt=${producto.nameProduct}></td><td>${producto.nameProduct}</td><td>${producto.currency}</td><td>${producto.quantity}</td><td>${producto.price}</td></tr>`;
+    content += `<tr><td style='padding:2px 8px'><img src=${process.env.NEXT_URL_BASE}${producto.imageProduct} width="130" alt=${producto.nameProduct}></td><td style='padding:2px 8px'>${producto.nameProduct}</td><td style='padding:2px 8px'>${producto.quantity}</td><td style='padding:2px 8px'>${producto.price}</td><td style='padding:2px 8px'>${producto.currency}</td></tr>`;
   });
 
   content += "</tbody></table>";
