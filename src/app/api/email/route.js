21b6641f956d4f91a43a84a40ed75238a089e7df
HafidOD <haifido1403@gmail.com>
2023-12-12 12:40:40 -0500

    modify emails

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -50,9 +50,9 @@ export async function POST(req) {
     // if (items.user.email != "masteruser@marriott.com") {
     const mailOptions = {
       from: `"Web2Print" <${process.env.SMTP_USER}>`,
-      to: `hafid@tachuela.mx`,
+      // to: `hafid@tachuela.mx`,
       // to: `${items.user.email}, marriott@gruporegio.mx, paloma.berumen@marriott.com, Asenath.araque@marriott.com, Amanda.k.perez@marriott.com, carlos.olguin@marriott.com, hafid@tachuela.mx`,
-      // to: `${items.user.email}, marriott@gruporegio.mx`,
+      to: `${items.user.email}, marriott@gruporegio.mx`,
       subject: "Solicitud de pedido Web2Print",
       html: emailContent,
     };
